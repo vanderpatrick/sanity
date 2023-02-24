9d5c2d439ff55bc38b3053a08d347e2ca9141cb6
vanderpatrick <patrick123321123@icloud.com>
2023-02-24 17:15:35 +0000

    initial commit for backend

---
+++ shareme_backend/schemas/user.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'user',
+    title: 'User',
+    type: 'document',
+    fields: [
+        {
+            name: 'userNamae',
+            title: 'UserName',
+            type: 'string'
+        },
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'string'
+        }
+    ]
+}
